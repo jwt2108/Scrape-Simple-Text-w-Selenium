a66cc97922bffa500030aac2856af6139d9c2d83
jwt2108 <125056411+jwt2108@users.noreply.github.com>
2024-02-13 19:37:25 +0000

    Auto Login Script

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
+import time
 def get_driver():
   options = webdriver.ChromeOptions()
   options.add_argument('disable-inforbars')
@@ -9,12 +10,16 @@ def get_driver():
   options.add_experimental_option('excludeSwitches', ['enable-automation'])
   options.add_argument('disable-blink-features=AutomationControlled')
   driver = webdriver.Chrome(options=options)
-  driver.get("http://automated.pythonanywhere.com")
+  driver.get("http://automated.pythonanywhere.com/login/")
   return driver
 
 def main():
   driver = get_driver()
-  element = driver.find_element(by='xpath', value="/html/body/div[1]/div/h1[1]")
-  return element.text
+  driver.find_element(by='id', value="id_username").send_keys('automated')
+  time.sleep(2)
+  driver.find_element(by='id', value="id_password").send_keys('automatedautomated' + Keys.RETURN)
+  time.sleep(3)
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  print(driver.current_url)
 
 print(main())
